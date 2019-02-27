477fb20bcc23b679d2007f4b292d70cd56573c4e
Default <aiell016@maclab-119.uwp.edu>
2019-02-26 18:56:26 -0600

    initial

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -13,6 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        let firstFrame = CGRect(x: 160, y:240, width:100, height:150)
+//        let firstView = UIView(frame: firstFrame)
+//        firstView.backgroundColor = UIColor.blue
+//        view.addSubview(firstView)
+//        print ("view did load")
+//        
+//        let secondFrame = CGRect(x: 20, y:30, width:20, height:20)
+//        let secondView = UIView(frame: secondFrame)
+//        secondView.backgroundColor = UIColor.green
+//        view.addSubview(secondView)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
