85f2d2ae310b467f95de42577b7bb3d740b2c5fb
Default <aiell016@maclab-119.uwp.edu>
2019-03-05 20:42:20 -0600

    done maybe?

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -8,30 +8,85 @@
 
 import UIKit
 
-class ConversionViewController : UIViewController {
+class ConversionViewController : UIViewController, UITextFieldDelegate {
     // I will use as a data output field
     @IBOutlet var celsiusLabel: UILabel!
     
     @IBOutlet var textField: UITextField!
     
+    var farenheitValue: Measurement<UnitTemperature>?{
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimum = 0
+        nf.maximum = 1
+        return nf
+    }()
+    
+    
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let farenheitValue = farenheitValue {
+            return farenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    
     // I want to be notified when sonmoeone enters a value in the text field
     @IBAction func fahrenheitFieldEditingChanged(_ textField:  UITextField) {
         
-        if let text = textField.text, !text.isEmpty {
+        if let text = textField.text, let value = Double(text) {
+            farenheitValue = Measurement(value: value, unit: .fahrenheit)
             
-            celsiusLabel.text = text
         } else {
-            celsiusLabel.text = "???"
+            farenheitValue = nil
         }
         
-        
     }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+    
+            celsiusLabel.text =
+                numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateCelsiusLabel()
+    }
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        let existingTextHasDecimalSeperator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeperator = string.range(of: ".")
+        if existingTextHasDecimalSeperator != nil,
+            replacementTextHasDecimalSeperator != nil {
+            return false
+            
+        } else {
+            return true
+        }
+    }
+    
     
     // Dismiss the keyboard
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
         textField.resignFirstResponder()
     }
-    
     
     
     
