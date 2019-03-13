c0010b3f3ce6890be424d907985fe9f11421d228
Aiello, Anthony M (aiell016) <aiell016@maclab-79.uwp.edu>
2019-03-12 21:14:18 -0500

    has a bug on selector line

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -8,10 +8,12 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 
 class MapViewControler : UIViewController {
 
+    var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,11 +21,52 @@ class MapViewControler : UIViewController {
     
     }
     
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        print("ConversionViewController loaded its view.")
-//        updateCelsiusLabel()
-//    }
+    override func loadView() {
+        // create a map view
+        mapView = MKMapView ()
+        // set it as the main view
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor)
+//        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+//        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        func mapTypeChanged(_ segControl: UISegmentedControl) {
+            switch segmentedControl.selectedSegmentIndex {
+                
+            case 0:
+                mapView.mapType = .standard
+            case 1:
+                mapView.mapType = .hybrid
+            case 2:
+                mapView.mapType = .satellite
+            default:
+                break
+            }
+        }
+        
+        segmentedControl.addTarget(self, action: #selector(MapViewControler.mapTypeChanged(_:) for: .valueChanged)
+        
+        
+        
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+        
+    }
+    
+
     
     
     override func viewWillAppear(_ animatedx: Bool) {
